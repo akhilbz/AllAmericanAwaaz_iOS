01ba0f1b9011653ab96a92cbb8055c450671cfe9
eshub1 <eshitha.bangray@gmail.com>
2025-03-10 03:23:10 -0500

    all teams page + singular team page done

---
+++ A3_IOS/TeamsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TeamsViewController.swift
+//  A3_IOS
+//
+//  Created by Eshitha B on 3/10/25.
+//
+
+import UIKit
+
+class TeamsViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+    
+}
